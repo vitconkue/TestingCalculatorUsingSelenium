953592e69990d5f9ac6bcb1ddd8674e7045942cc
AN VÕ THIỆN <voan0935995700@gmail.com>
2021-12-26 15:07:12 +0700

    finish test

--- TestingTestShepNz/TestSuite.cs
+++ TestingTestShepNz/TestSuite.cs
@@ -11,42 +11,53 @@ using Xunit.Abstractions;
 
 namespace TestingTestShepNz
 {
-    public class SuiteTests : IDisposable {
+    public class TestSuite : IDisposable
+    {
         private readonly ITestOutputHelper _testOutputHelper;
         private readonly string _webLink = "https://testsheepnz.github.io/BasicCalculator.html";
-        private IWebDriver Driver {get; set;}
-        private Dictionary<string, string> Vars {get; set;}
+        private IWebDriver Driver { get; set; }
+        private Dictionary<string, string> Vars { get; set; }
 
-        private IWebDriver WebDriverFactory(string browserName) => browserName switch
+        private IWebDriver WebDriverFactory(string browserName)
         {
-            "firefox" => new FirefoxDriver(),
-            "chrome" => new ChromeDriver(),
-            "edge" => new EdgeDriver(),
-            _ => new ChromeDriver()
-        }; 
-        public SuiteTests(ITestOutputHelper testOutputHelper)
+            return browserName switch
+            {
+                "firefox" => new FirefoxDriver(),
+                "chrome" => new ChromeDriver(),
+                "edge" => new EdgeDriver(),
+                _ => new ChromeDriver()
+            };
+        }
+
+        public TestSuite(ITestOutputHelper testOutputHelper)
         {
             _testOutputHelper = testOutputHelper;
             Vars = new Dictionary<string, string>()
             {
                 {"numberAnswerField", ""},
-                {"test1",""}
+                {"errorMsgField", ""}
             };
         }
+
         public void Dispose()
         {
             Driver.Quit();
         }
 
-        public async Task RunInput(string number1,
+        private async Task RunInput(string number1,
             string number2,
             string buildVersion,
             string operation,
-            bool checkIntegerOnly, 
+            bool checkIntegerOnly,
             bool willCalculate,
             bool willClear)
         {
             Driver.Navigate().GoToUrl(_webLink);
+            Driver.FindElement(By.Id("selectBuild")).Click();
+            {
+                var dropdown = Driver.FindElement(By.Id("selectBuild"));
+                dropdown.FindElement(By.XPath($"//option[. = '{buildVersion}']")).Click();
+            }
             Driver.FindElement(By.Id("number1Field")).Click();
             Driver.FindElement(By.Id("number1Field")).SendKeys(number1);
             Driver.FindElement(By.Id("number2Field")).Click();
@@ -57,21 +68,25 @@ namespace TestingTestShepNz
                 dropdown.FindElement(By.XPath($"//option[. = '{operation}']")).Click();
             }
             Driver.FindElement(By.Id("calculateButton")).Click();
-            await Task.Delay(3000);
-            var text = Driver.FindElement(By.Id("numberAnswerField")).GetAttribute("value");
+            //await Task.Delay(3000);
+            string text = "";
+            while (text == "")
+            {
+                text = Driver.FindElement(By.Id("numberAnswerField")).GetAttribute("value");
+            }
             Vars["numberAnswerField"] = text;
             Driver.FindElement(By.Id("clearButton")).Click();
         }
-        
-        
+
+
         [Theory]
         [MemberData(nameof(GetAllEnvironment))]
-        public async Task Test1(string browserName, string buildVersion)
+        public async Task ValidMultiplyInput(string browserName, string buildVersion)
         {
             Driver = WebDriverFactory(browserName);
-            await 
-                RunInput("3", "5", "", "Multiply", false, true, false); 
-            Assert.Equal("15",Vars["numberAnswerField"]);
+            await
+                RunInput("3", "5", buildVersion, "Multiply", false, true, false);
+            Assert.Equal("15", Vars["numberAnswerField"]);
         }
 
 
@@ -79,28 +94,23 @@ namespace TestingTestShepNz
         public static List<object[]> GetAllEnvironment()
         {
             var allEnvi = new List<object[]>();
-            List<string> browsers = new List<string>
+            var browsers = new List<string>
             {
                 "firefox", "chrome", "edge"
             };
 
-            List<string> buildVersions = new List<string>
+            var buildVersions = new List<string>
             {
-                "Prototype", "1", "2", "3", "4", "5", "6", "7", "8", "9"
+                //"Prototype", "1", "2", "3", "4", "5", "6", "7", "8", "9"
+                 "8" 
             };
 
             foreach (var browser in browsers)
-            {
-                foreach (var buildVersion in buildVersions)
-                {
-                    allEnvi.Add(new object[] {browser,buildVersion});
-                }
-            }
+            foreach (var buildVersion in buildVersions)
+                allEnvi.Add(new object[] {browser, buildVersion});
 
 
             return allEnvi;
         }
-
-      
     }
 }
